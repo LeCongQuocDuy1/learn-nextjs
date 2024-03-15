69f35e1157f2467347871fe7e067d6b8a4854296
LeCongQuocDuy <duy9117@gmai.com>
2024-03-15 10:45:13 +0700

    edit, delete invoice and login, logout

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers Page',
+};
 
 const CustomersPage = () => {
   return <div>CustomersPage</div>;
